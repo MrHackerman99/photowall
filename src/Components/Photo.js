d8152731d496e6676188c2d7365efdfaf3934f57
MrHackerman99 <lewisclay9@gmail.com>
2022-12-11 13:55:41 +0000

    Commit 2

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,10 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTyes from "prop-types";
 
-class Photo extends Component {
-  render() {
-    const post = this.props.post;
-    return <figure className="figure"> </figure>;
-  }
+function Photo(props) {
+  const post = props.post;
+  return (
+    <figure className="figure">
+      <img className="photo" src={post.imageLink} alt={post.description} />
+      <figcaption>
+        <p> {post.description} </p>
+      </figcaption>
+      <div className="button-container">
+        <button
+          className="remove-button"
+          onClick={() => {
+            props.onRemovePhoto(post);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </figure>
+  );
 }
+
+Photo.propTyes = {
+  post: PropTyes.object.isRequired,
+  onRemovePhoto: PropTyes.func.isRequired,
+};
 
 export default Photo;
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import Photo from "./Photo";
+import PropTyes from "prop-types";
 
-class Photowall extends Component {
-  render() {
-    return (
-      <div className="photoGrid">
-        {this.props.posts.map((post, index) => (
-          <Photo key={index} post={post} />
-        ))}
-      </div>
-    );
-  }
+function PhotoWall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
+      ))}
+    </div>
+  );
 }
 
-export default Photowall;
+PhotoWall.propTyes = {
+  posts: PropTyes.array.isRequired,
+  onRemovePhoto: PropTyes.func.isRequired,
+};
+
+export default PhotoWall;
