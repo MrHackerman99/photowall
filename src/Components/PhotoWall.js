6428adb473f7ef10546908d7f6ccca2b20236208
MrHackerman99 <lewisclay9@gmail.com>
2022-12-11 15:28:15 +0000

    commit 3

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Photo from "./Photo";
 import PropTyes from "prop-types";
+import { Link } from "react-router-dom";
+//anchor tag, href attribute
 
 function PhotoWall(props) {
   return (
-    <div className="photoGrid">
-      {props.posts.map((post, index) => (
-        <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
-      ))}
+    <div>
+      <Link className="addIcon" to="/AddPhoto">
+        Click Me
+      </Link>
+      <div className="photoGrid">
+        {props.posts
+          .sort(function (x, y) {
+            return y.id - x.id;
+          })
+          .map((post, index) => (
+            <Photo
+              key={index}
+              post={post}
+              onRemovePhoto={props.onRemovePhoto}
+            />
+          ))}
+      </div>
     </div>
   );
 }
